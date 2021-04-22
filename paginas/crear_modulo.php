32bcef81b0be88ceeda43bc7b4bc9ba6a52ff3e3
javierrollan <jrollanb@gmail.com>
2021-04-22 17:27:18 +0200

    Separacion codigo php en distintos ficheros

---
+++ paginas/crear_modulo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Proyecto</title>
+</head>
+<body>
+
+	<h2>Creacion Modulo</h2>
+
+	<p>Creacion de modulo Puppet que contendra la estructura de directorios y archivos.</p>
+
+	<form action="../scripts/php/c_modulo.php" method="POST">
+		<input type="text" name="username" />
+		<input type="text" name="pwd" />
+		<input type="text" name="nombre_modulo" />
+		<br>
+		<input type="submit" />
+	</form>
+
+	
+</body>
+</html>
