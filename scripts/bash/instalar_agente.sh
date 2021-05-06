37916930a2e52c262905be5de3342a80ba7d7919
javierrollan <jrollanb@gmail.com>
2021-05-06 17:41:30 +0200

    Funcion Instalar Terminada

--- scripts/bash/instalar_agente.sh
+++ scripts/bash/instalar_agente.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 sshpass -p $1 ssh -T -o StrictHostKeyChecking=no $2@$3 <<EOF
-echo "$1" | sudo -S apt update -y
 cd ~
-touch prueba.txt
+wget https://apt.puppet.com/puppet7-release-focal.deb
+echo "$1" | sudo -S dpkg -i puppet7-release-focal.deb
+echo "$1" | sudo -S apt update -y
+echo "$1" | sudo -S apt-get install puppet-agent -y
 exit
 EOF
