b39664a4630447e5c57aead6033baa1211c54317
javierrollan <jrollanb@gmail.com>
2021-05-17 17:22:45 +0200

    Mejora Scripts Bash

--- scripts/bash/insertar_nodo.sh
+++ scripts/bash/insertar_nodo.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sudo su <<EOF
-$1
-sed -i "3i$2 	$3" /etc/hosts
+echo "$1" | sudo -S sed -i "3i$2 	$3" /etc/hosts
+
+sshpass -p $1 ssh -T -o StrictHostKeyChecking=no $2@$3 <<EOF
+
+echo "$1" | sudo -S sed -i "3i192.168.1.50	puppetserver puppet" /etc/hosts
+
+echo "$1" | sudo -S sed -i "3i$3 	$2" /etc/hosts
+
+exit
 EOF
--- scripts/bash/instalar_agente.sh
+++ scripts/bash/instalar_agente.sh
@@ -6,5 +6,7 @@ wget https://apt.puppet.com/puppet7-release-focal.deb
 echo "$1" | sudo -S dpkg -i puppet7-release-focal.deb
 echo "$1" | sudo -S apt update -y
 echo "$1" | sudo -S apt-get install puppet-agent -y
+echo "$1" | sudo -S systemctl enable puppet.service
+echo "$1" | sudo -S systemctl start puppet.service
 exit
 EOF
