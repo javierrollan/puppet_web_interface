edddff0edd949b6ba9cf5efde3e80c7a7fc464a0
Javier Rollan Blanco <jrollanb@gmail.com>
2021-06-01 11:06:18 +0200

    Editor de Manifiesto creado

---
+++ paginas/manifiesto.php
@@ -0,0 +1,56 @@
+<?php 
+	include ("../scripts/php/include/conexiondb.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Proyecto</title>
+	<script type="text/javascript" src="../scripts/js/jquery.js"></script>
+</head>
+<body>
+
+	<div class="navegador">
+		<ul>
+			<li><a href="../index.php">Resumen</a></li>
+			<li><a href="nodos.php">Nodos</a></li>
+			<li><a href="paquete.php">Paquetes</a></li>
+			<li><a href="instalar.php">Instalar</a></li>
+			<li><a href="certificados.php">Certificados</a></li>
+			<li><a href="ejecutar.php">Ejecutar</a></li>
+            <li><a href="manifiesto.php">Ejecutar</a></li>
+			<li><a href="crear_modulo.php">Modulos</a></li>
+			<li><a href="crear_clases.php">Clases</a></li>
+			<li><a href="edicion_clase.php">Edicion</a></li>
+		</ul>
+	</div>
+
+	<h2>Edicion de Manifiesto:</h2>
+
+    <?php 
+        $fichero = "/etc/puppetlabs/code/environments/production/manifests/manifiesto.pp";
+
+        if(isset($_POST['datos'])) {
+            file_put_contents($fichero, $_POST['datos']);
+
+            header('Location: manifiesto.php');
+        }
+
+        $texto = file_get_contents($fichero);
+    ?>
+
+	<form action="" method="POST">
+		<br>
+		<p>Editar Manifiesto:</p>
+		<br>
+		<textarea name="datos" id="" cols="30" rows="10"><?php $salida = htmlspecialchars($texto); echo $salida; ?></textarea>
+        <input type="submit">
+    </form>
+	
+	<?php 
+		mysqli_close($conexion);
+	?>
+
+</body>
+</html>
--- scripts/bash/insertar_nodo.sh
+++ scripts/bash/insertar_nodo.sh
@@ -4,7 +4,7 @@ echo "$1" | sudo -S sed -i "3i$2 	$3" /etc/hosts
 
 sshpass -p $1 ssh -T -o StrictHostKeyChecking=no $2@$3 <<EOF
 
-echo "$1" | sudo -S sed -i "3i192.168.1.50	puppetserver puppet" /etc/hosts
+echo "$1" | sudo -S sed -i "3i192.168.2.124	envy puppet" /etc/hosts
 
 echo "$1" | sudo -S sed -i "3i$3 	$2" /etc/hosts
 
